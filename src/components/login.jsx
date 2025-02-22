0ff2cb965141461d5251c4ae0ed9ac039589f0d3
Saptarshi Paul <saptarshi2027paul@gmail.com>
2025-02-22 14:07:00 +0530

    Google and Github Auths Added

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -4,7 +4,14 @@ import { useState } from "react";
 import '../app.css'
 import '../index.css'
 import app from '../firebase'
+import { signInWithPopup,  GoogleAuthProvider } from "firebase/auth";
+import {  GithubAuthProvider  , signInWithRedirect} from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+const GitHubprovider = new GithubAuthProvider();
+
 const auth = getAuth(app);
+
 const Login = () => {
   const [email, useEmail] = useState("");
   const [password, usePassword] = useState("");
@@ -12,14 +19,28 @@ const Login = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((value) => alert("Sign In Sucess"))
       .catch((error) => alert("User Not Found"))
-}
-return (
+  }
+  const SignInGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then(() => alert("Google SignIn Successful"))
+      .catch((error) => alert("Googele SignIn Not Successful"))
+  }
+  const SignInGithub = () => {
+    signInWithRedirect(auth, GitHubprovider)
+    .then(() => alert("Github SignIn Successful"))
+    .catch((error) => alert("Github SignIn Not Succesfull"))
+  }
+  return (
     <>
       <h1>Enter Your Email</h1>
       <input type="email" placeholder="Enter Your Email " className="auth" onChange={(e) => useEmail(e.target.value)} value={email} ></input>
       <h1>Set Your Password</h1>
-      <input type="email" placeholder="Set  Your Password " className="auth" value={password} onChange={(e) => usePassword(e.target.value)}></input>
-      <button className="btn" onClick={SignInUser}>SignIn</button>
+      <input type="password" placeholder="Set  Your Password " className="auth" value={password} onChange={(e) => usePassword(e.target.value)}></input>
+      <div className="btns">
+        <button className="btn" onClick={SignInUser}>SignIn</button>
+        <button className="btn-2" onClick={SignInGoogle}>Sign In With Google</button>
+        <button className="btn-2" onClick={SignInGithub}>Sign In With Github</button>
+      </div>
     </>
   )
 }
--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import '../app.css'
 import '../index.css'
+import { GoogleAuthProvider , signInWithPopup, } from "firebase/auth";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 import app from '../firebase'
-const auth = getAuth(app) ;
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
 const Register = () => {
-const [email , setEmail] = useState("");
-const [password , setPassword] = useState("");
-const SignUpUser = () => {
-  createUserWithEmailAndPassword(auth , email , password)
-  .then((value) => alert("User Created Succesfully"))
-  .catch((error) => alert("User is Not Created"));
-}
-return (
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const SignUpUser = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((value) => alert("User Created Succesfully"))
+      .catch((error) => alert("User is Not Created"));
+  }
+  const SignUpWithGoogle = () => {
+    signInWithPopup(auth, provider)
+    .then(() => alert("Sign Up Sucessfull"))
+    .catch(() => alert("Sign Up Successfull") ) 
+  };
+  return (
     <>
-    <h1>Enter Your Email</h1>
-    <input type="email" placeholder="Enter Your Email " className="auth" value={email}  onChange={(e) => setEmail(e.target.value)} ></input>
-    <h1>Enter Your Password</h1>
-    <input type="password" placeholder="Enter  Your Password " className="auth"  value={password}  onChange={(e) =>setPassword(e.target.value)}></input>
-    <button className="btn" onClick={SignUpUser}>SignUp</button>
+      <h1>Enter Your Email</h1>
+      <input type="email" placeholder="Enter Your Email " className="auth" value={email} onChange={(e) => setEmail(e.target.value)} ></input>
+      <h1>Enter Your Password</h1>
+      <input type="password" placeholder="Enter  Your Password " className="auth" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+      <div className="btns">
+      <button className="btn" onClick={SignUpUser}>SignUp</button>
+      <button className="btn-2" onClick={SignUpWithGoogle}>Sign Up with Google</button>
+      </div>
     </>
   )
 }
